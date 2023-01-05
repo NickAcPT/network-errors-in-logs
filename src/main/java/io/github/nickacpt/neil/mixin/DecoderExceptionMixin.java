e62100097c52b8d908dee9125b6c4a6d12d6b33b
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-01-05 16:45:39 +0000

    Target all ctors

--- src/main/java/io/github/nickacpt/neil/mixin/DecoderExceptionMixin.java
+++ src/main/java/io/github/nickacpt/neil/mixin/DecoderExceptionMixin.java
@@ -8,9 +8,27 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(DecoderException.class)
 public class DecoderExceptionMixin {
-	@Inject(at = @At("TAIL"), method = "<init>(Ljava/lang/String;)V", remap = false)
+	@Inject(at = @At("RETURN"), method = "<init>(Ljava/lang/String;)V", remap = false)
 	private void init(String message, CallbackInfo ci) {
-		System.err.println("Decoder exception was thrown, here's a stacktrace:");
-		new RuntimeException(message).printStackTrace();
+		System.err.println("Decoder exception was thrown (with message), here's a stacktrace:");
+		new RuntimeException("Decoder exception: " + message).printStackTrace();
+	}
+
+	@Inject(at = @At("RETURN"), method = "<init>()V", remap = false)
+	private void init(CallbackInfo ci) {
+		System.err.println("Decoder exception was thrown (without message), here's a stacktrace:");
+		new RuntimeException("Decoder exception").printStackTrace();
+	}
+
+	@Inject(at = @At("RETURN"), method = "<init>(Ljava/lang/Throwable;)V", remap = false)
+	private void init(Throwable throwable, CallbackInfo ci) {
+		System.err.println("Decoder exception was thrown (with exception), here's a stacktrace:");
+		new RuntimeException("Decoder exception", throwable).printStackTrace();
+	}
+
+	@Inject(at = @At("RETURN"), method = "<init>(Ljava/lang/String;Ljava/lang/Throwable;)V", remap = false)
+	private void init(String message, Throwable throwable, CallbackInfo ci) {
+		System.err.println("Decoder exception was thrown (with message and exception), here's a stacktrace:");
+		new RuntimeException("Decoder exception: " + message, throwable).printStackTrace();
 	}
 }
