e62100025218dc80b25cf90c346d20d460114c74
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-01-05 16:34:05 +0000

    Fix method being static

--- src/main/java/io/github/nickacpt/neil/mixin/DecoderExceptionMixin.java
+++ src/main/java/io/github/nickacpt/neil/mixin/DecoderExceptionMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(DecoderException.class)
 public class DecoderExceptionMixin {
 	@Inject(at = @At("TAIL"), method = "<init>(Ljava/lang/String;)V", remap = false)
-	private static void init(String message, CallbackInfo ci) {
+	private void init(String message, CallbackInfo ci) {
 		System.err.println("Decoder exception was thrown, here's a stacktrace:");
 		new RuntimeException(message).printStackTrace();
 	}
